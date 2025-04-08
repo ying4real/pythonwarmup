8f0661ebd6a2ac5f269f0f0398e4eb16eff501e0
ying4real <jchang12@berklee.edu>
2025-04-08 19:42:36 -0400

    level 7 complete

--- python_review.py
+++ python_review.py
@@ -1,3 +1,31 @@
-x = type(3)
-y = type("Hello!")
-z = type(True)
+# Put the Data Type in the comment
+my_integer = 42                     # Integer
+my_integer_type = type(my_integer)
+print(my_integer_type)
+my_float = 3.14                     # Float
+my_float_type = type(my_float)
+print(my_float_type)
+my_string = "Hello, World!"         # String
+my_string_type = type(my_string)
+print(my_string_type)
+my_boolean_true = True              # Boolean
+my_boolean_ture_type = type(my_boolean_true)
+print(my_boolean_ture_type)
+my_boolean_false = False            # Boolean
+my_boolean_false_type = type(my_boolean_false)
+print(my_boolean_false_type)
+my_none_value = None                # None
+my_none_value_type = type(my_none_value)
+print(my_none_value_type)
+my_negative_integer =  -7           # Integer
+my_negative_integer_type = type(my_negative_integer)
+print(my_negative_integer_type)
+my_large_float = 1.23456789e10      # Float
+my_large_float_type = type(my_large_float)
+print(my_large_float_type)
+my_single_character = 'A'           # String
+my_single_character_type = type(my_single_character)
+print(my_single_character_type)
+my_empty_string = ""                # String
+my_empty_string_type = type(my_empty_string)
+print(my_empty_string_type)
