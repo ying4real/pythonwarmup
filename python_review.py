9b46fcdb82584cc7eb12b9f69f5a3a97b6a1c783
ying4real <jchang12@berklee.edu>
2025-04-08 19:24:10 -0400

    level 5 complete

--- python_review.py
+++ python_review.py
@@ -1,10 +1,10 @@
-example =  "Im a string" # The variable called example is now holding the string value "Im a string"
-a = 3 # The variable called a is now holding the integer value 3.
-b = 4.0 # The variable called b is now holding the float value 4.0.
-c = True # The variable called c is now holding the boolean value True.
-d = False # The variable called d is now holding the boolean value False.
-e = "Hey!" # The variable called e is now holding the string value "Hey!"".
-f = None # The variable called f is now holding the none value None.
-age = 32 # The variable called age is now holding the integer value 32.
-name = "Insert your name here" # The variable called name is now holding the string value "Insert your name here".
-instrument = "Insert your instrument here" # The variable called instrument is now holding the string value "Insert your instrument here".
+'''
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (aage, Age and AGE are there different variables)
+
+
+$$$Variable = "bad"
+
+'''
